5c6e52bfa62a33c1019782f124f2ba12874427db
GiFelisardo <giffelisardo@gmail.com>
2026-09-11 12:12:27 -0300

    add swagger

---
+++ src/main/java/com/senaibackend/rental_eventos/config/Swagger.java
@@ -0,0 +1,20 @@
+package com.senaibackend.rental_eventos.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(
+info = @Info(
+title = "Rental Eventos",
+version = "1.0",
+description = "Locações de equipamentos"
+)
+)
+
+public class Swagger {
+    
+}
